007b4bfdac193b57ba00ec4c8d167a02db842f61
Esme577 <esmeraldaquezada57@gmail.com>
2025-09-26 17:21:18 -0700

    debuggin tailwind

---
+++ src/components/cards/CardComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// prop: imgs text
+export default function Card () {
+return(
+  <div className="bg-green-500 text-white p-4">
+  Tailwind is now working ✅
+</div>
+)
+}
+
+         
+    
+
