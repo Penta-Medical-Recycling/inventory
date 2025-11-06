4debaca0255891da39e4bee4c64d245d2b9c3cf6
Esme577 <esmeraldaquezada57@gmail.com>
2025-11-05 21:28:57 -0800

    changed the layout to percentages that allows for the crads to be responsive to the screen size I still have to go back and edit the responiveness of the images

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -13,30 +13,21 @@ import "../../App.css";
 
 
 export default function Card({partName,imgSrc, }) {
-  // const props = [
-
-  //   { id: 1, partName: "Double Adapters - Male", img: "https://drive.google.com/thumbnail?id=1HI-BP4exGIb7sADpZ-FQS2QFx_bzC3YV" },
-  //   // { id: 2, partName: "Double Adapter - Female", img: "https://drive.google.com/thumbnail?id=1A07AHZskJ80j8SyHgdA02jm3Ro21n6p4" },
-  //   // { id: 3, partName: "Double Adapters - Male", img: "./src/public/image.png" },
-  //   // { id: 4, partName: "Double Adapter - Female", img: "./src/public/image.png" },
-  //   // { id: 5, partName: "Double Adapter2 - Female", img: "./src/public/image.png" },
-  //   // { id: 6, partName: "Double Adapter4 - Female", img: "./src/public/image.png" },
-
-  // ];
-
-
+ 
   
   return (
 // whole screen Container
-    <div  >
-      {/* Card */}
-          <div className="w-full h-[349px]  border-2 border-gray-200 rounded-2xl shadow-md p-6 flex flex-col items-center hover:shadow-xl">
+   
+    
+ 
+         
+            <div className=" w-4/4 h-[349px]  border-2 border-gray-200 rounded-2xl shadow-md p-6 flex flex-col items-center justify-between hover:shadow-xl ">
 
 
             {/* img div in div put tailwind code */}
-            <div className="mb-3 flex-1 flex items-center justify-center max-h-[200px] object-contain overflow-hidden" >
+            <div className="w-[238px] h-[181px] mb-4 flex items-center justify-center  object-contain overflow-hidden" >
               <img
-              className=" w-[120px] h-[120px] object-contain"
+              className=" w-full h-full object-contain"
                src={imgSrc}
                 alt={partName}
               />
@@ -48,7 +39,8 @@ export default function Card({partName,imgSrc, }) {
               {partName}
             </div>
           </div>
-      </div>
+       
+  
     
   );
 }
