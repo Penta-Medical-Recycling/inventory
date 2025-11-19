38f3f451b37314af3200a7d2d592e5ca3b6b3974
Jordan <jordinholenhart@gmail.com>
2025-11-18 17:06:53 -0800

    removed unnecesary text

--- src/components/sidebar-filters/Extremity.jsx
+++ src/components/sidebar-filters/Extremity.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Extremity = ({ extremity, setExtremity }) => {
   return (
-    <div className="filter-section flex flex-col gap-3">
+    <div className="flex flex-col gap-3">
       <label className="mb-1 text-[#4A4A4A] text-2xl">Select Extremity</label>
       <div className="is-fullwidth w-100 rounded-xl border flex overflow-hidden">
         {["All", "Lower", "Upper"].map((option) => (
